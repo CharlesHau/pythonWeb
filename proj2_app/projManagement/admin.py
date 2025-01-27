8d490c783dd57e4277c65d937b384446409cdaa6
CharlesHau <128699891+CharlesHau@users.noreply.github.com>
2025-01-27 11:22:25 +0100

    Delete proj2_app directory

--- proj2_app/projManagement/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from projManagement.models import Client, Mission
-# Register your models here.
-admin.site.register(Client)
-admin.site.register(Mission)
--- proj2_app/sanstitre2.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 27 08:46:56 2025
-
-@author: charl
-"""
-
-    
